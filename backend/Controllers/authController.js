f96ba0bb278d9eb0ab4ecdde67a4b1e874d42da5
rafiakbar13 <muhammadrafia13@gmail.com>
2023-11-17 19:32:36 +0700

    added authentication

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,105 @@
+import User from "../models/UserSchema.js";
+import Doctor from "../models/DoctorSchema.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+const generateToken = (user) => {
+  return jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "15d" }
+  );
+};
+export const register = async (req, res) => {
+  const { email, password, name, role, photo, gender } = req.body;
+  try {
+    let user = null;
+    if (role === "patient") {
+      user = await User.findOne({ email });
+    } else if (role === "doctor") {
+      user = await Doctor.findOne({ email });
+    }
+    // Check if user already exists
+    if (user) {
+      return res.status(400).json({ message: "User Already exist" });
+    }
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hassPassword = await bcrypt.hash(password, salt);
+
+    if (role === "patient") {
+      user = new User({
+        name,
+        email,
+        password: hassPassword,
+        photo,
+        gender,
+        role,
+      });
+    }
+    if (role === "doctor") {
+      user = new Doctor({
+        name,
+        email,
+        password: hassPassword,
+        photo,
+        gender,
+        role,
+      });
+    }
+
+    await user.save();
+    res
+      .status(201)
+      .json({ success: true, message: "User Registered Successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Internal server error, Try again" });
+  }
+};
+export const login = async (req, res) => {
+  const { email } = req.body;
+  try {
+    let user = null;
+    const patient = await User.findOne({ email });
+    const doctor = await Doctor.findOne({ email });
+
+    if (patient) {
+      user = patient;
+    }
+    if (doctor) {
+      user = doctor;
+    }
+
+    // check if user  exist or not
+    if (!user) {
+      return res.status(404).json({ message: "User does not exist" });
+    }
+    // compare password
+    const isPasswordMath = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordMath) {
+      return res
+        .status(400)
+        .json({ status: false, message: "Invalid credentials" });
+    }
+
+    // get token
+
+    const token = generateToken(user._id, user.role);
+    const { password, role, appointments, ...rest } = user._doc;
+    res.status(201).json({
+      status: true,
+      message: "Successfully Login",
+      token,
+      data: { ...rest, role: user.role },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: true,
+      message: "Failed to login, Try again",
+    });
+  }
+};
